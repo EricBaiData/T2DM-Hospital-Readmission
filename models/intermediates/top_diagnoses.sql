7e79e61017b1c034a6ef18ea07f37f4e9b4e1a50
EricBaiBusiness <ericbai.business@gmail.com>
2024-11-11 11:51:31 -0500

    change to top 3 diagnoses

--- models/intermediates/top_diagnoses.sql
+++ models/intermediates/top_diagnoses.sql
@@ -1,10 +1,8 @@
 SELECT
     hadm_id,
-    MAX(CASE WHEN seq_num = 1 THEN icd_code END) AS diagnosis_1,
-    MAX(CASE WHEN seq_num = 2 THEN icd_code END) AS diagnosis_2,
-    MAX(CASE WHEN seq_num = 3 THEN icd_code END) AS diagnosis_3,
-    MAX(CASE WHEN seq_num = 4 THEN icd_code END) AS diagnosis_4,
-    MAX(CASE WHEN seq_num = 5 THEN icd_code END) AS diagnosis_5
+    , MAX(CASE WHEN seq_num = 1 THEN icd_code END) AS diagnosis_1
+    , MAX(CASE WHEN seq_num = 2 THEN icd_code END) AS diagnosis_2
+    , MAX(CASE WHEN seq_num = 3 THEN icd_code END) AS diagnosis_3
 FROM
     {{ ref('stg_diagnoses_icd') }}
 GROUP BY
